0e273e69b67ff83b83da7a9afe4420fefdd7b3e6
machenfei <1984534457@qq.com>
2022-05-22 21:11:20 +0800

    master- 测试类输出时间(2)

--- src/main/java/com/tyut/mcf/security_study/test/Maintest.java
+++ src/main/java/com/tyut/mcf/security_study/test/Maintest.java
@@ -4,5 +4,6 @@ public class Maintest {
     public static void main(String[] args) {
         System.out.println("666");
         System.out.println("2022-02-22");
+        System.out.println("2022-02-22(02)");
     }
 }
