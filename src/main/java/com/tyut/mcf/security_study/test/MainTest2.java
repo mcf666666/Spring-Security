b3345bd906d74de364027f39808f77fd9f433bf0
machenfei <machfei@yonyou.com>
2022-08-11 10:59:19 +0800

    My First

---
+++ src/main/java/com/tyut/mcf/security_study/test/MainTest2.java
@@ -0,0 +1,11 @@
+package com.tyut.mcf.security_study.test;
+
+/**
+ * @author :mcf
+ * @date : 2022/8/11
+ */
+public class MainTest2 {
+    public static void main(String[] args) {
+        System.out.println("The First Code in YonYOu!");
+    }
+}
